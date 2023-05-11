a26036a2786db177f83b0f903214bb2f7e068cf4
Md. Golam Kausar <golam@mirinfosys.com>
2023-05-11 15:01:57 +0600

    retrofit get with picasso in recyclerview

---
+++ app/src/main/java/com/kausar/retrofitdemo/MainActivity.java
@@ -0,0 +1,65 @@
+package com.kausar.retrofitdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.kausar.retrofitdemo.adapter.CustomAdapter;
+import com.kausar.retrofitdemo.model.RetroPhoto;
+import com.kausar.retrofitdemo.network.GetDataService;
+import com.kausar.retrofitdemo.network.RetrofitClientInstance;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private CustomAdapter adapter;
+    private RecyclerView recyclerView;
+    ProgressDialog progressDoalog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressDoalog = new ProgressDialog(MainActivity.this);
+        progressDoalog.setMessage("Loading....");
+        progressDoalog.show();
+
+        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
+        Call<List<RetroPhoto>> call = service.getAllPhotos();
+        call.enqueue(new Callback<List<RetroPhoto>>() {
+            @Override
+            public void onResponse(Call<List<RetroPhoto>> call, Response<List<RetroPhoto>> response) {
+                progressDoalog.dismiss();
+                generateDataList(response.body());
+                
+            }
+
+            @Override
+            public void onFailure(Call<List<RetroPhoto>> call, Throwable t) {
+                progressDoalog.dismiss();
+                Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+    }
+
+    private void generateDataList(List<RetroPhoto> photoList) {
+        recyclerView = findViewById(R.id.customRecyclerView);
+        adapter = new CustomAdapter(this,photoList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/kausar/retrofitdemo/network/GetDataService.java
@@ -0,0 +1,14 @@
+package com.kausar.retrofitdemo.network;
+
+import com.kausar.retrofitdemo.model.RetroPhoto;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GetDataService {
+
+    @GET("/photos")
+    Call<List<RetroPhoto>> getAllPhotos();
+}
